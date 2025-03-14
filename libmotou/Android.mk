bd9b170fb492926985fa6b8edbd849693f5fb9c7
Federico Saccà <feduss96@gmail.com>
2025-03-14 22:57:55 +0100

    libmotou: fixed LOCAL_PATH, removed LOCAL_SRC_FILES and added libutils to LOCAL_SHARED_LIBRARIES
    Change-Id: I1e8ee808440ee501a032a18818861057ae0c1f7f

--- libmotou/Android.mk
+++ libmotou/Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := $(ANDROID_BUILD_TOP)
+LOCAL_PATH := $(call my-dir)
 
 # VectorImpl
 
@@ -21,39 +21,12 @@ include $(CLEAR_VARS)
 LU_PATH := system/core/libutils
 LOC_PATH := device/motorola/omap4-common/libmotou
 
-LOCAL_SRC_FILES := \
-        $(LU_PATH)/BlobCache.cpp \
-        $(LU_PATH)/CallStack.cpp \
-        $(LU_PATH)/FileMap.cpp \
-        $(LU_PATH)/JenkinsHash.cpp \
-        $(LU_PATH)/LinearTransform.cpp \
-        $(LU_PATH)/Log.cpp \
-        $(LU_PATH)/Looper.cpp \
-        $(LU_PATH)/NativeHandle.cpp \
-        $(LU_PATH)/Printer.cpp \
-        $(LU_PATH)/ProcessCallStack.cpp \
-        $(LU_PATH)/PropertyMap.cpp \
-        $(LU_PATH)/RefBase.cpp \
-        $(LU_PATH)/SharedBuffer.cpp \
-        $(LU_PATH)/Static.cpp \
-        $(LU_PATH)/StopWatch.cpp \
-        $(LU_PATH)/String8.cpp \
-        $(LU_PATH)/String16.cpp \
-        $(LU_PATH)/SystemClock.cpp \
-        $(LU_PATH)/Threads.cpp \
-        $(LU_PATH)/Timers.cpp \
-        $(LU_PATH)/Tokenizer.cpp \
-        $(LU_PATH)/Trace.cpp \
-        $(LU_PATH)/Unicode.cpp \
-        $(LU_PATH)/misc.cpp \
-        $(LOC_PATH)/MotoVectorImpl.cpp
-
 LOCAL_MODULE := libmotou
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES += \
 	external/safe-iop/include \
 	system/core/libutils
 
-LOCAL_SHARED_LIBRARIES := libbacktrace libcutils libdl liblog
+LOCAL_SHARED_LIBRARIES := libbacktrace libcutils libdl liblog libutils
 
 include $(BUILD_SHARED_LIBRARY)
