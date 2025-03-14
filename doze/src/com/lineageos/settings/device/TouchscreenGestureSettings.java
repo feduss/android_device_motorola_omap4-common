1cfb25537c9552f8be8c46a4bb1f24d243d7fe0a
Federico Saccà <feduss96@gmail.com>
2025-03-14 22:52:57 +0100

    doze: renamed cyanogen to lineage
    Change-Id: I63ec481b0320f6a53a970afb207f691c225f9d10

--- doze/src/com/lineageos/settings/device/TouchscreenGestureSettings.java
+++ doze/src/com/lineageos/settings/device/TouchscreenGestureSettings.java
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.linegaeos.settings.device;
 
-import org.cyanogenmod.internal.util.ScreenType;
+import org.lineageos.internal.util.ScreenType;
 
 import android.app.ActionBar;
 import android.os.Bundle;
